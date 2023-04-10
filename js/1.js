b5af2303e40bab8034f9d7ed93e284ef6b7df887
Mauricio <mauricioutn2017@gmail.com>
2023-04-09 22:14:08 -0300

    Version 1.0 lista

---
+++ js/1.js
@@ -0,0 +1,30 @@
+function cpu(){
+    const cpu = Math.floor(Math.random() * 5) + 1;
+    return cpu;
+}
+//Defino los botones necesarios
+let botonComenzar = document.getElementById(`comenzar`);
+let botonEnviar = document.getElementById(`enviar`);
+let entradaTexto = document.getElementById(`entrada`);
+// Manejador de Eventos
+botonComenzar.addEventListener("click",ocultarMostrar);
+botonEnviar.addEventListener("click",enviar);
+
+//Defino las funciones para cada boton 
+function ocultarMostrar(){
+botonComenzar.style.display  =`none`;
+botonEnviar.style.display=`block`;
+entradaTexto.style.display=`block`;
+}
+function enviar(){
+let valor = entradaTexto.value;
+let a=cpu();
+let b= parseInt(valor);
+console.log(a);
+console.log(b);
+if(a==b){
+alert(`Ganaste`);
+}else{
+    alert(`Perdiste`);
+}
+}
